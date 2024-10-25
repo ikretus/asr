92a3ff199640fe0ce0648725ec31b15a19f4ab3e
ikretus <ikretus@gmail.com>
2024-10-25 12:02:50 +0300

    added process control

--- conf.py
+++ conf.py
@@ -1,24 +1,20 @@
 import json
 import os
 
-DAYS_TO_SCAN = 8
-DAYS_TO_TEST = 8
-FETCH_MANY = 100
-MAX_CPU = 2
-N_THREAD = "2"
-ROOT_DIR = os.path.join(os.environ["HOME"], "devel/asr")
+ROOT_DIR: str = os.path.join(os.environ["HOME"], "devel/asr")
+
+with open(os.path.join(ROOT_DIR, "conf.json")) as ff:
+    CONF = json.loads(ff.read())
+
 SAMPLE_WAV = {
-    "en": os.path.join(ROOT_DIR, "../whisper.cpp/samples/sample_en.wav"),
-    "ru": os.path.join(ROOT_DIR, "../whisper.cpp/samples/sample_ru.wav")
+    "en": os.path.join(ROOT_DIR, "../dnn/whisper.cpp/samples/sample_en.wav"),
+    "ru": os.path.join(ROOT_DIR, "../dnn/whisper.cpp/samples/sample_ru.wav")
 }
-SLEEP_TIME = 1.0
 WHISPER = {
-    "exec": ["whisper", "-p", "1", "-t", N_THREAD, "-ng", "-oj", "-l"],
+    "exec": ["whisper", "-p", "1", "-t", str(CONF["n_thread"]), "-ng", "-oj", "-l"],
     "model": {
-        "path": os.path.join(ROOT_DIR, "../whisper.cpp/models"),
-        "1": {"lev0": 0.15, "lev1": 0.19, "lev2": 0.92, "lev3": 2.1, "lev4": 3.9},
-        "2": {"lev0": 0.18, "lev1": 0.25, "lev2": 0.59, "lev3": 1.4, "lev4": 2.5},
+        "path": os.path.join(ROOT_DIR, "../dnn/whisper.cpp/models"),
+        1: {"lev0": 0.15, "lev1": 0.19, "lev2": 0.92, "lev3": 2.1, "lev4": 3.9},
+        2: {"lev0": 0.18, "lev1": 0.25, "lev2": 0.59, "lev3": 1.4, "lev4": 2.5},
     }
 }
-with open(os.path.join(ROOT_DIR, "conf.json")) as ff:
-    DB_CONF = json.loads(ff.read())
--- main.py
+++ main.py
@@ -1,107 +1,134 @@
-import conf
 import json
 import os
 import random
-import shutil
 import subprocess
 import sys
 import time
 import uuid
 
-from datetime import datetime, timedelta
-from glob import glob
-from operator import itemgetter
+from conf import CONF, ROOT_DIR, SAMPLE_WAV, WHISPER
+from datetime import datetime
 from psycopg2 import Error, connect
 
+SLEEP_TIME = 5.0
 TODAY = datetime.now()
 
 
-def gen_data(connector=None):
-    for i in range(conf.DAYS_TO_TEST):
-        dt = (TODAY - timedelta(i)).strftime("%y%m%d")
-        os.makedirs(dt, exist_ok=True)
+def wlog(vid, code, msg):
+    level = {"i": "INFO", "w": "WARN", "e": "ERROR"}.get(code, "INFO")
+    print("%s %s (%s) %s" % (datetime.now().strftime("%y%m%d:%H%M%S"), vid, level, msg), flush=True)
+
+
+def gen_data(connector, n=8):
+    dt = TODAY.strftime("%y%m%d")
+    os.makedirs(dt, exist_ok=True)
+    for i in range(n):
         auid = str(uuid.uuid4())
         lang = ("ru", "en")[random.randint(0, 1)]
         model = "lev%s" % random.randint(0, 4)
-        if connector is not None:
-            if not create_auid(connector, auid, lang, model):
-                print("AUID(%s) not created in DATABASE" % auid)
-        os.popen("cp %s %s/%s_%s_%s.%s" % (conf.SAMPLE_WAV[lang], dt, auid, lang, model, "win"))
+        create_auid(connector, auid, lang, model, "test")
+        os.popen("cp %s %s/%s_%s_%s.wav" % (SAMPLE_WAV[lang], dt, auid, lang, model))
         os.wait()
-
-
-def get_auid(fn):
-    return fn.split("_", 1)[0][7:]
-
-
-def duration(fn):
-    m = fn.rsplit("_", 1)[1][:4]
-    return os.path.getsize(fn) * conf.WHISPER["model"][conf.N_THREAD][m] / 32000.
 
 
 def fext(fn, ext="wav"):
     return fn[:-3] + ext
 
 
-def lang(fn):
-    return fn.rsplit("_", 2)[1]
+def get_auid(fn):
+    return fn.split("_", 1)[0][7:]
 
 
-def model(fn):
-    return os.path.join(conf.WHISPER["model"]["path"], fext(fn.rsplit("_", 1)[1], "bin"))
+def get_model(fn):
+    return os.path.join(WHISPER["model"]["path"], fext(fn.rsplit("_", 1)[1], "bin"))
 
 
-def input_files():
-    inp = list()
-    for i in (0, 1):
-        os.makedirs((TODAY + timedelta(i)).strftime("%y%m%d"), exist_ok=True)
-    for i in range(conf.DAYS_TO_SCAN):
-        dt = (TODAY - timedelta(i)).strftime("%y%m%d")
-        if os.path.exists(dt):
-            inp.extend(glob(os.path.join(dt, "*.win")))
-    return [it[0] for it in sorted([(fn, duration(fn)) for fn in inp], key=itemgetter(1))]
+def get_input(connector, mode):
+    query = "SELECT auid, lang, model, loaded FROM " + CONF[mode]["table"] + " WHERE processing IS NULL ORDER BY loaded"
+    try:
+        with connector:
+            with connector.cursor() as cur:
+                cur.execute(query)
+                inp = ["%s/%s_%s_%s.wav" % (it[3].strftime("%y%m%d"), it[0], it[1], it[2]) for it in cur.fetchall()]
+    except Error as err:
+        wlog("db", "e", str(err).replace("\n", ". "))
+        return list()
+    n_inp = len(inp)
+    inp = [it for it in inp if os.path.exists(it)]
+    wlog("loaded", "i", "local = %s, total = %s" % (len(inp), n_inp))
+    return inp if mode == "test" or len(inp) == 0 else [inp[0]]
 
 
-def run(cmds, connector=None, mode="test", it=0):
-    def remove_finished(ps, connector=None, mode="test"):
-        new_ps = list()
-        for fn, proc in ps:
-            auid, status = get_auid(fn), proc.poll()
-            if status is None:
-                new_ps.append((fn, proc))
-            elif status == 0:
-                print("DONE: %s" % fn, flush=True)
-                os.rename(fn, fext(fn))
-                if connector is not None:
-                    if not update_status(connector, auid, "success", mode):
-                        print("STATUS(%s) not updated to SUCCESS" % auid)
-                    with open(fext(fn, "json")) as ff:
-                        if not update_status(connector, auid, json.loads(ff.read()), mode):
-                            print("JSON(%s) not loaded into DATABASE" % auid)
-            else:
-                print("FAIL: %s" % fn, flush=True)
-                os.rename(fn, fext(fn, "failed"))
-                if connector is not None:
-                    if not update_status(connector, auid, "failed", mode):
-                        print("STATUS(%s) not updated to FAILED" % auid)
-                    with open(fext(fn, "log")) as ff:
-                        if not update_status(connector, auid, ff.read(), mode):
-                            print("LOG(%s) not loaded into DATABASE" % auid)
-        return new_ps
+def wait_proc(fn, proc, connector, mode):
+    auid, status = get_auid(fn), proc.poll()
+    if status is None:
+        return True
+    elif status == 0:
+        wlog(auid, "i", "success")
+        with open(fext(fn, "json")) as ff:
+            update_status(connector, auid, "success", json.loads(ff.read()), mode)
+    else:
+        with open(fext(fn, "log")) as ff:
+            wlog(auid, "e", ff.read().replace("\n", ". "))
+        update_status(connector, auid, "failed", "whisper error", mode)
+    return False
 
+
+def run(cmds, connector, mode, it=0):
     ps = list()
     for cmd in cmds:
+        auid = get_auid(cmd[it])
         ps.append((cmd[it], subprocess.Popen(cmd, stdout=open(fext(cmd[it], "log"), "w"), stderr=subprocess.STDOUT)))
-        if connector is not None:
-            if not update_status(connector, get_auid(cmd[it]), "processing", mode):
-                print("STATUS(%s) not updated to PROCESSING" % get_auid(cmd[it]))
-        print("RUN: %s" % cmd[it], flush=True)
-        while len(ps) >= conf.MAX_CPU:
-            time.sleep(conf.SLEEP_TIME)
-            ps = remove_finished(ps, connector)
+        wlog(auid, "i", "processing")
+        update_status(connector, auid, "processing", datetime.now(), mode)
+        while len(ps) >= CONF["max_cpu"]:
+            time.sleep(SLEEP_TIME)
+            ps = [(fn, proc) for (fn, proc) in ps if wait_proc(fn, proc, connector, mode)]
     while len(ps) > 0:
-        time.sleep(conf.SLEEP_TIME)
-        ps = remove_finished(ps, connector)
+        time.sleep(SLEEP_TIME)
+        ps = [(fn, proc) for (fn, proc) in ps if wait_proc(fn, proc, connector, mode)]
+
+
+def check_proc(connector, mode):
+    ps, fn_pid = os.popen("ps --no-header -C %s -o pid,cmd" % WHISPER["exec"][0]).readlines(), dict()
+    os.wait()
+    for it in ps:
+        it = it.split()
+        if len(it) == 16:
+            fn_pid[it[11]] = int(it[0])
+
+    query = "SELECT auid, lang, model, loaded, processing, attempt FROM " + CONF[mode]["table"] + \
+            " WHERE processing IS NOT NULL AND failed IS NULL AND success IS NULL"
+    try:
+        with connector:
+            with connector.cursor() as cur:
+                cur.execute(query)
+                ps = cur.fetchall()
+    except Error as err:
+        wlog("db", "e", str(err).replace("\n", ". "))
+        return None
+    for it in ps:
+        fn = "%s/%s_%s_%s.wav" % (it[3].strftime("%y%m%d"), it[0], it[1], it[2])
+        if os.path.exists(fn):
+            ttl = 2 * (os.path.getsize(fn) * WHISPER["model"][CONF["n_thread"]][it[2]] / 32000.)
+            if (TODAY - it[4]).total_seconds() > ttl:
+                pid = fn_pid.pop(fn, None)
+                if pid is not None:
+                    try:
+                        os.kill(pid, 9)
+                        wlog(it[0], "w", "killed: processing too long")
+                        update_status(connector, it[0], "failed", "killed", mode)
+                    except Exception as err:
+                        wlog("processing", "e", str(err).replace("\n", ". "))
+                else:
+                    if it[5] == 1:
+                        wlog(it[0], "w", "resumed: 2nd attempt")
+                        update_status(connector, it[0], "processing", None, mode)
+                    else:
+                        wlog(it[0], "w", "killed: 2nd attempt failed")
+                        update_status(connector, it[0], "failed", "killed", mode)
+    wlog("processing", "i", "local = %s, total = %s" % (len(fn_pid), len(ps)))
+    return len(fn_pid) < CONF["max_cpu"]
 
 
 def init_connector(conf, create_table=True):
@@ -110,7 +137,7 @@ def init_connector(conf, create_table=True):
                             host=conf["host"], port=conf["port"])
         connector.autocommit = True
     except Error as err:
-        print(err)
+        wlog("db", "e", str(err).replace("\n", ". "))
         return None
     if create_table:
         query = """
@@ -118,6 +145,7 @@ def init_connector(conf, create_table=True):
             auid uuid NOT NULL PRIMARY KEY,
             lang char(2) NOT NULL,
             model char(4) NOT NULL,
+            attempt smallint NOT NULL DEFAULT 0,
             loaded timestamp NOT NULL DEFAULT now(),
             processing timestamp, failed timestamp, success timestamp,
             log text, result jsonb, target jsonb
@@ -127,69 +155,60 @@ def init_connector(conf, create_table=True):
                 with connector.cursor() as cur:
                     cur.execute(query)
         except Error as err:
-            print(err)
+            wlog("db", "e", str(err).replace("\n", ". "))
             return None
     return connector
 
 
-def create_auid(connector, auid, lang, model, mode="test"):
-    table = conf.DB_CONF[mode]["table"]
+def create_auid(connector, auid, lang, model, mode="data"):
     try:
         with connector:
             with connector.cursor() as cur:
-                query = "INSERT INTO " + table + "(auid, lang, model) VALUES (%s, %s, %s)"
+                query = "INSERT INTO " + CONF[mode]["table"] + "(auid, lang, model) VALUES (%s, %s, %s)"
                 cur.execute(query, (auid, lang, model))
     except Error as err:
-        print(err)
+        wlog("db", "e", str(err).replace("\n", ". "))
         return False
     return True
 
 
-def update_status(connector, auid, val, mode="test"):
-    table = conf.DB_CONF[mode]["table"]
+def update_status(connector, auid, key, val, mode):
+    table = CONF[mode]["table"]
     try:
         with connector:
             with connector.cursor() as cur:
-                if isinstance(val, dict):
-                    query = "UPDATE " + table + " SET result = %s WHERE auid = %s"
-                    cur.execute(query, (json.dumps(val["transcription"]), auid))
-                elif val in ("processing", "failed", "success"):
-                    query = "UPDATE " + table + " SET " + val + " = %s WHERE auid = %s"
-                    cur.execute(query, (datetime.now(), auid))
-                else:
-                    query = "UPDATE " + table + " SET log = %s WHERE auid = %s"
+                if key == "processing":
+                    query = "UPDATE " + table + " SET processing = %s, attempt = attempt + 1 WHERE auid = %s"
                     cur.execute(query, (val, auid))
+                elif key == "failed":
+                    query = "UPDATE " + table + " SET failed = %s, log = %s WHERE auid = %s"
+                    cur.execute(query, (datetime.now(), val, auid))
+                elif key == "success":
+                    query = "UPDATE " + table + " SET success = %s, result = %s WHERE auid = %s"
+                    cur.execute(query, (datetime.now(), json.dumps(val["transcription"]), auid))
+                elif key == "target":
+                    query = "UPDATE " + table + " SET target = %s WHERE auid = %s"
+                    cur.execute(query, (json.dumps(val), auid))
     except Error as err:
-        print(err)
-        return False
-    return True
+        wlog(auid, "e", str(err).replace("\n", ". "))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or sys.argv[1] not in conf.DB_CONF:
-        print("USAGE: %s [test|data]" % sys.argv[0])
+    mode = "test" if len(sys.argv) == 2 and sys.argv[1].isdecimal() else "data"
+    connector = init_connector(CONF[mode])
+    if connector is None:
         sys.exit(1)
 
-    mode, root = sys.argv[1], os.path.join(conf.ROOT_DIR, sys.argv[1])
-    connector = init_connector(conf.DB_CONF[mode])
-
-    if connector is None and mode != "test":
-        print("EXIT: database connector unavailable in production mode")
-        sys.exit(1)
-
-    if mode == "test":
-        shutil.rmtree(root, ignore_errors=True)
-
+    root = os.path.join(ROOT_DIR, mode)
     os.makedirs(root, exist_ok=True)
     os.chdir(root)
 
     if mode == "test":
-        gen_data(connector)
+        gen_data(connector, int(sys.argv[1]))
+    elif not check_proc(connector, mode):
+        sys.exit(1)
 
-    inp = [conf.WHISPER["exec"] + [lang(fn), "-f", fn, "-m", model(fn), "-of", fn[:-4]] for fn in input_files()]
-    if inp:
-        print("input queue length = %s" % len(inp), flush=True)
-        run(inp, connector, mode, 10)
+    run([WHISPER["exec"] + [fn.rsplit("_", 2)[1], "-f", fn, "-m", get_model(fn), "-of", fn[:-4]]
+         for fn in get_input(connector, mode)], connector, mode, 10)
 
-    if connector is not None:
-        connector.close()
+    connector.close()
